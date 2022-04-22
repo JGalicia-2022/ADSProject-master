f1ba9ac08d4a4c29e20d41dd38c82f4c285dae3f
Josue Galicia <102382526+JGalicia-2022@users.noreply.github.com>
2022-04-22 13:31:20 -0600

    Actualizacion de grupos

--- ADSProject/Models/GruposViewModel.cs
+++ ADSProject/Models/GruposViewModel.cs
@@ -12,20 +12,23 @@ namespace ADSProyect.Models
         [Display(Name = "ID Grupo")]
         public int idGrupo { get; set; }
 
+
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
         [Display(Name = "ID Carrera")]
         public int idCarrera { get; set; }
 
         [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
         [Display(Name = "ID Materia")]
         public int idMateria { get; set; }
-        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
 
+        [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
         [Display(Name = "ID Profesor")]
         public int idProfesor { get; set; }
+     
         [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
-
         [Display(Name = "Ciclo")]
         public int ciclo { get; set; }
+
         [Required(ErrorMessage = Constants.REQUIRED_FIELD)]
         [Display(Name = "Año")]
         public int anio { get; set; }
