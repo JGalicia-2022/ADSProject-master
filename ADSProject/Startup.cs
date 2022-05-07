d8b3d79741307a20255081007f7cd8f8ab2d4459
Josue Galicia <102382526+JGalicia-2022@users.noreply.github.com>
2022-05-06 20:36:02 -0600

    Se realizo la implementacion de cadena de conexion, y carpeta Data y ApplicationDbContext

--- ADSProject/Startup.cs
+++ ADSProject/Startup.cs
@@ -1,7 +1,9 @@
 using ADSProject.Repository;
+using ADSProyect.Data;
 using ADSProyect.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,11 @@ namespace ADSProject
             services.AddSingleton<IProfesorRepository, ProfesorRepository>();
             services.AddSingleton<ICarreraRepository, CarreraRepository>();
             services.AddSingleton<IGrupoRepository, GrupoRepository>();
+
+            //Configuracion de DB
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
